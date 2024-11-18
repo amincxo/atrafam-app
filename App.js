6705dbc00de8216c95632d34d60273ab4f06eda9
unknown <amincxo@gmail.com>
2024-11-18 23:43:59 +0330

    app update

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AuthProvider } from './src/context/AuthContext';
+import SplashScreen from './src/screens/SplashScreen';
+import SearchScreen from './src/screens/SearchScreen';
+import MapScreen from './src/screens/MapScreen';
+import LoginScreen from './src/screens/auth/LoginScreen';
+import SignUpSelctor from './src/screens/SignUpSelctor';
+import SellerSignUp from './src/screens/auth/SellerSignUp';
+import UserSignUp from './src/screens/auth/UserSignUp';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+        <SafeAreaProvider>
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown:false}} >
+            <Stack.Screen name='Splash' component={SplashScreen} />
+            <Stack.Screen name='SignUpSelctor' component={SignUpSelctor} />
+            <Stack.Screen name='SellerSignUp' component={SellerSignUp} />
+            <Stack.Screen name='UserSignUp' component={UserSignUp} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Search" component={SearchScreen} />
+            <Stack.Screen name="Map" component={MapScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+        </SafeAreaProvider>
+    </AuthProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
